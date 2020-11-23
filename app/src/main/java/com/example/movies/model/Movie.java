28ac707d5382157c70800f784f09efccb2134dc9
Renlov <a.pimenov95@mail.ru>
2020-11-23 21:56:14 +0300

    add MovieAdapter and add Picasso method

--- app/src/main/java/com/example/movies/model/Movie.java
+++ app/src/main/java/com/example/movies/model/Movie.java
@@ -1,6 +1,36 @@
 package com.example.movies.model;
 
-//Класс для содержание всех свойств объекта Movie, методы
+//Класс для содержание всех свойств объекта Movie
 public class Movie {
+    private String title;
+    private String posterUrl;
+    private String year;
 
+    public Movie(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
