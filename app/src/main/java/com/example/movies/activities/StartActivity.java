ef0a169ab1c7a31bc83e8841b4a518b8ddc75c08
Renlov <a.pimenov95@mail.ru>
2020-12-05 19:52:07 +0300

    add icon

---
+++ app/src/main/java/com/example/movies/activities/StartActivity.java
@@ -0,0 +1,14 @@
+package com.example.movies.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.movies.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
